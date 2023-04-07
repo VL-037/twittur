37c3a8a19e6c5e9ee8c5a665f286af96b4babc81
Vincent Low <vincent.low@gdn-commerce.com>
2023-04-08 03:48:05 +0700

    Dockerize Twittur using dockerfile & docker-compose

--- src/main/java/vincentlow/twittur/TwitturApplication.java
+++ src/main/java/vincentlow/twittur/TwitturApplication.java
@@ -2,10 +2,10 @@ package vincentlow.twittur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"vincentlow.twittur"})
+@PropertySource("classpath:application-dev.properties")
 public class TwitturApplication {
 
   public static void main(String[] args) {
